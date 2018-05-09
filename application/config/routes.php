f3731d2dee8772881cd4f1c60da86106f102c5d5
mrivanovic <37665599+mrivanovic@users.noreply.github.com>
2018-05-09 14:15:43 +0200

    css

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,8 +49,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
+$route['login'] = 'category/login';
 $route['default_controller'] = 'Category';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 
-$route['category/login'] = '/login';
