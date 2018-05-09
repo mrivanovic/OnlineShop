114bdd306d8e6095fea7922b37ee339ee9efb595
mrivanovic <37665599+mrivanovic@users.noreply.github.com>
2018-05-10 00:13:47 +0200

    frontend

--- application/views/signUp.php
+++ application/views/signUp.php
@@ -3,7 +3,11 @@
         <table>
             <tr>
                 <th><h1>Kupci</h1></th>
+            </tr>
+            <tr>
                 <td><p>Registracija za kupce. Registrovani korisnici mogu iskljucivo da vrse usluge kupovine.</p></td>
+            </tr>
+            <tr>
                 <td><button type="submit"><i class="fa fa-user-plus" aria-hidden="true"></i>Sing Up</button></td>
             </tr>
         </table>
@@ -12,7 +16,11 @@
         <table>
             <tr>
                 <th><h1>Prodavci</h1></th>
+            </tr>
+            <tr>
                 <td><p>Registracija za prodavce. Registrovani korisnici mogu iskljucivo da vrse usluge prodaje.</p></td>
+            </tr>
+            <tr>
                 <td><button type="submit"><i class="fa fa-user-plus" aria-hidden="true"></i>Sing Up</button></td>
             </tr>
         </table>
