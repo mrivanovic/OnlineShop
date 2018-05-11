170939f20f57b3ee5c4ace19c3d59c7c1d0ff530
mrivanovic <37665599+mrivanovic@users.noreply.github.com>
2018-05-11 08:12:39 +0200

    baza

--- shop.sql
+++ shop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 10, 2018 at 01:20 PM
+-- Generation Time: May 10, 2018 at 08:57 AM
 -- Server version: 10.1.30-MariaDB
 -- PHP Version: 7.2.1
 
@@ -30,15 +30,13 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `buyer` (
   `id` int(11) NOT NULL,
-  `name` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
-  `lastname` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `ime` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `prezime` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
   `mail` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
   `password` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
-  `country` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
-  `city` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
-  `adress` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
-  `tel` varchar(15) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `dateofbirth` date DEFAULT NULL
+  `drzava` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `grad` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `adresa` varchar(200) COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
@@ -48,26 +46,8 @@ CREATE TABLE `buyer` (
 --
 
 CREATE TABLE `category` (
-  `id` int(11) NOT NULL,
-  `ime` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `parent_id` int(100) DEFAULT NULL
+  `id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Dumping data for table `category`
---
-
-INSERT INTO `category` (`id`, `ime`, `parent_id`) VALUES
-(1, 'Laptop', NULL),
-(2, 'Desktop', NULL),
-(3, 'Laptop', NULL),
-(4, 'Desktop', NULL),
-(5, 'komponente', NULL),
-(6, 'tableti', NULL),
-(7, 'oprema', NULL),
-(8, 'monitori', NULL),
-(9, 'graficke', 5),
-(10, 'procesori', 5);
 
 -- --------------------------------------------------------
 
@@ -77,10 +57,7 @@ INSERT INTO `category` (`id`, `ime`, `parent_id`) VALUES
 
 CREATE TABLE `comments` (
   `id` int(11) NOT NULL,
-  `product_id` int(11) NOT NULL,
-  `buyer_id` int(11) NOT NULL,
-  `content` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
-  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  `tekst` varchar(2000) COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
@@ -91,10 +68,7 @@ CREATE TABLE `comments` (
 
 CREATE TABLE `images` (
   `id` int(11) NOT NULL,
-  `name` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
-  `mine-tipe` int(11) NOT NULL,
-  `extension` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
-  `path` varchar(20) COLLATE utf8_unicode_ci NOT NULL
+  `naziv` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
@@ -105,24 +79,9 @@ CREATE TABLE `images` (
 
 CREATE TABLE `messages` (
   `id` int(11) NOT NULL,
-  `from_id` int(11) NOT NULL,
-  `to_id` int(11) NOT NULL,
-  `order_id` int(11) NOT NULL,
-  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  `naslov` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `tekst` varchar(2000) COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `order-products`
---
-
-CREATE TABLE `order-products` (
-  `id` int(11) NOT NULL,
-  `order_id` int(11) NOT NULL,
-  `product_id` int(11) NOT NULL,
-  `quantiti` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -132,39 +91,9 @@ CREATE TABLE `order-products` (
 
 CREATE TABLE `orders` (
   `id` int(11) NOT NULL,
-  `buyer_id` int(11) NOT NULL,
-  `sellers_id` int(11) NOT NULL,
-  `datetime` datetime NOT NULL,
-  `sent` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
-  `delivered` int(20) NOT NULL
+  `kolicina` int(11) NOT NULL,
+  `komentar` varchar(250) COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `posts`
---
-
-CREATE TABLE `posts` (
-  `id` int(11) NOT NULL,
-  `sellers_id` int(11) NOT NULL,
-  `tittle` varchar(20) NOT NULL,
-  `content` int(200) NOT NULL,
-  `timestemp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `product-image`
---
-
-CREATE TABLE `product-image` (
-  `id` int(11) NOT NULL,
-  `product_id` int(11) NOT NULL,
-  `image_id` int(11) NOT NULL,
-  `main` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -173,46 +102,30 @@ CREATE TABLE `product-image` (
 --
 
 CREATE TABLE `products` (
-  `name` varchar(50) DEFAULT NULL,
-  `descriptions` int(250) NOT NULL,
-  `category_id` int(11) NOT NULL,
-  `sellers_id` int(11) NOT NULL,
-  `price` int(11) NOT NULL,
-  `id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `id` int(11) NOT NULL,
+  `naziv` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `opis` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `cena` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `nacindostave` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `cenadostave` varchar(200) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `ratings`
+-- Table structure for table `salesman`
 --
 
-CREATE TABLE `ratings` (
+CREATE TABLE `salesman` (
   `id` int(11) NOT NULL,
-  `product_id` int(11) NOT NULL,
-  `buyer_id` int(11) NOT NULL,
-  `rate` int(11) NOT NULL,
-  `timestemp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `sellers`
---
-
-CREATE TABLE `sellers` (
-  `id` int(11) NOT NULL,
-  `name` varchar(20) NOT NULL,
-  `lastname` varchar(20) NOT NULL,
-  `mail` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL,
-  `country` varchar(50) NOT NULL,
-  `city` varchar(50) NOT NULL,
-  `adress` varchar(50) NOT NULL,
-  `tel` varchar(20) NOT NULL,
-  `dateofbirth` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `ime` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `prezime` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `mail` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `drzava` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `grad` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `adresa` varchar(200) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Indexes for dumped tables
@@ -228,15 +141,13 @@ ALTER TABLE `buyer`
 -- Indexes for table `category`
 --
 ALTER TABLE `category`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `parent_id` (`parent_id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `comments`
 --
 ALTER TABLE `comments`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `products_id` (`product_id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `images`
@@ -248,57 +159,24 @@ ALTER TABLE `images`
 -- Indexes for table `messages`
 --
 ALTER TABLE `messages`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `order_id` (`order_id`);
-
---
--- Indexes for table `order-products`
---
-ALTER TABLE `order-products`
   ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `orders`
 --
 ALTER TABLE `orders`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `buyer_id` (`buyer_id`),
-  ADD KEY `sell` (`sellers_id`);
-
---
--- Indexes for table `posts`
---
-ALTER TABLE `posts`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `seller_id` (`sellers_id`);
-
---
--- Indexes for table `product-image`
---
-ALTER TABLE `product-image`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `product_id` (`product_id`),
-  ADD KEY `image_id` (`image_id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `products`
 --
 ALTER TABLE `products`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `sellers_id` (`category_id`);
+  ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `ratings`
+-- Indexes for table `salesman`
 --
-ALTER TABLE `ratings`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `prduct_id` (`product_id`),
-  ADD KEY `buyers_id` (`buyer_id`);
-
---
--- Indexes for table `sellers`
---
-ALTER TABLE `sellers`
+ALTER TABLE `salesman`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -315,7 +193,7 @@ ALTER TABLE `buyer`
 -- AUTO_INCREMENT for table `category`
 --
 ALTER TABLE `category`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `comments`
@@ -336,27 +214,9 @@ ALTER TABLE `messages`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `order-products`
---
-ALTER TABLE `order-products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `posts`
---
-ALTER TABLE `posts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `product-image`
---
-ALTER TABLE `product-image`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
@@ -366,72 +226,10 @@ ALTER TABLE `products`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `ratings`
+-- AUTO_INCREMENT for table `salesman`
 --
-ALTER TABLE `ratings`
+ALTER TABLE `salesman`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `sellers`
---
-ALTER TABLE `sellers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `category`
---
-ALTER TABLE `category`
-  ADD CONSTRAINT `category_ibfk_1` FOREIGN KEY (`parent_id`) REFERENCES `category` (`id`) ON DELETE CASCADE;
-
---
--- Constraints for table `comments`
---
-ALTER TABLE `comments`
-  ADD CONSTRAINT `products_id` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`);
-
---
--- Constraints for table `messages`
---
-ALTER TABLE `messages`
-  ADD CONSTRAINT `order_id` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`);
-
---
--- Constraints for table `orders`
---
-ALTER TABLE `orders`
-  ADD CONSTRAINT `buyer_id` FOREIGN KEY (`buyer_id`) REFERENCES `buyer` (`id`),
-  ADD CONSTRAINT `sell` FOREIGN KEY (`sellers_id`) REFERENCES `sellers` (`id`);
-
---
--- Constraints for table `posts`
---
-ALTER TABLE `posts`
-  ADD CONSTRAINT `seller_id` FOREIGN KEY (`sellers_id`) REFERENCES `sellers` (`id`);
-
---
--- Constraints for table `product-image`
---
-ALTER TABLE `product-image`
-  ADD CONSTRAINT `image_id` FOREIGN KEY (`image_id`) REFERENCES `images` (`id`),
-  ADD CONSTRAINT `product_id` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`);
-
---
--- Constraints for table `products`
---
-ALTER TABLE `products`
-  ADD CONSTRAINT `category_id` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`),
-  ADD CONSTRAINT `sellers_id` FOREIGN KEY (`category_id`) REFERENCES `sellers` (`id`);
-
---
--- Constraints for table `ratings`
---
-ALTER TABLE `ratings`
-  ADD CONSTRAINT `buyers_id` FOREIGN KEY (`buyer_id`) REFERENCES `buyer` (`id`),
-  ADD CONSTRAINT `prduct_id` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
