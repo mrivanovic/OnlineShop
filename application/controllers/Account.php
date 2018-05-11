08d692698487552526d755f1209dd195c43bf785
mrivanovic <37665599+mrivanovic@users.noreply.github.com>
2018-05-11 09:27:53 +0200

    config

--- application/controllers/Account.php
+++ application/controllers/Account.php
@@ -5,10 +5,14 @@ class Account extends CI_Controller
     public function __construct()
     {
         parent:: __construct();
+        $this->load->model('');
     }
     public function loadView()
     {
         
     }
-    
+    public function register()
+    {
+        
+    }
 }
