84c90fc20371e0fe6abcffc3c92d16462fe474d7
mrivanovic <37665599+mrivanovic@users.noreply.github.com>
2018-05-14 11:34:39 +0200

    view seller

---
+++ application/views/sellerProfile.php
@@ -0,0 +1,84 @@
+
+<div id="sellProfNav">
+    <div class="left">
+        <a href="#">Add Advert</a>
+    </div>
+    <div class="center">
+        <a href=""><i class="far fa-envelope"></i></a>
+    </div>
+    <div class="right">
+        <a href="#">Advert View</a>
+    </div>
+</div>
+<div id="Seller">
+    <div class="sellerfleft">
+
+        <img src="<?php echo base_url();?>img/seller.png"><br>
+        <button type="file" name="img">Dodaj</button>
+    </div>
+    <div class="sellerright">
+        <table>
+            <tr>
+                <th colspan="2" style="background-color: grey; color: white;">Licni Podaci:</th>
+            </tr>
+            <tr>
+                <td></td>
+            </tr>
+            <tr>
+                <th>Name:</th>
+                <td><input type="text" value="Ljubisa" name="name" /></td>
+            </tr>
+            <tr>
+                <th>Lastname:</th>
+                <td><input type="text" value="Ivanovic" name="lastname" /></td>
+            </tr>
+            <tr>
+                <th>E-mail:</th>
+                <td><input type="text" value="ljubisa@gmail.com" name="mail" /></td>
+            </tr>
+            <tr>
+                <th>Tel:</th>
+                <td><input type="text" value="0643878442" name="tel" /></td>
+            </tr>
+            <tr>
+
+                <th>City:</th>
+                <td><input type="text" value="Priboj" name="city" /></td>
+            </tr>
+            <tr>
+                <th>Adress:</th>
+                <td><input type="text" value="22. Decembar 8" name="adress" /></td>
+            </tr>
+            <tr>
+                <th colspan="2"><input type="submit" value="Sacuvaj promene" /></th>
+            </tr>
+            <tr>
+                <th colspan="2" style="background-color: grey; color: white;">Promenite lozinku</th>
+            </tr>
+            <tr>
+                <td></td>
+            </tr>
+            <tr>
+                <th>Lozinka</th>
+                <td><input type="text" name="pass" /></td>
+            </tr>
+            <tr>
+                <th>Potvrdite lozinku</th>
+                <td><input type="text" name="pass1" /></td>
+            </tr>
+            <tr>
+                <th colspan="2"><input type="submit" value="Sacuvaj promene" /></th>
+            </tr>
+            <tr>
+                <th colspan="2" style="background-color: grey; color: white;">Izbrisi nalog</th>
+            </tr>
+            <tr>
+                <td></td>
+            </tr>
+            <tr>
+                <th colspan="2"><input type="submit" value="Izbrisi" /></th>
+            </tr>
+        </table>
+    </div>
+
+</div>
