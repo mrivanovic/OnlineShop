bfc48687c8341ad846d859993beb8acb456f15f2
Veljko <39084695+veljkophp@users.noreply.github.com>
2018-05-14 09:17:50 +0200

    SignuupP

--- application/models/AccountModel.php
+++ application/models/AccountModel.php
@@ -1,6 +1,6 @@
 <?php
 
-class AccountModel extends CI_Controller
+class AccountModel extends CI_Model
 {
     function __construct() {
         parent::__construct();
@@ -8,7 +8,26 @@ class AccountModel extends CI_Controller
     }
     public function SignUpP($data)
     {
-        $data['message'] = 'Data Inserted Successfully';
+        
         $this->db->insert('seller', $data);
+    }
+    
+    
+    
+     public function postojiEmail() {
+        $this->db->where('mail', $this->email);
+        $result = $this->db->get('seller');
+        if ($result->result())
+            return TRUE;
+        else
+            return FALSE;
+    }
+
+    public function ispravanpassword($password){
+        $this->db->where('mail', $this->username);
+        $this->db->where('password', $password);
+        $result->$this->db->get('seller');
+        $buyer=$result->row_array();
+          
     }
 }
--- application/views/signUpP.php
+++ application/views/signUpP.php
@@ -9,56 +9,56 @@
             <?php echo form_open('Account/registerP', 'method=POST'); ?>
 
             <tr>
-                    <th><?php echo form_label('Name:'); ?> <?php echo form_error('dname'); ?></th>
-                    <td><?php echo form_input(array('id' => 'dname', 'name' => 'dname')); ?></td>
-                </tr>
-                <tr>
-                    <th><?php echo form_label('Last Name:'); ?> <?php echo form_error('dlastname'); ?></th>
-                    <td><?php echo form_input(array('id' => 'dlastname', 'lastname' => 'dlastname')); ?></td>
-                </tr>
-                <tr>
-                    <th><?php echo form_label('Email:'); ?> <?php echo form_error('demail'); ?></th>
-                    <td><?php echo form_input(array('id' => 'demail', 'mail' => 'demail')); ?></td>
-                </tr>
-                <tr>
-                    <th><?php echo form_label('Password:'); ?> <?php echo form_error('dpassword'); ?></th>
-                    <td><?php echo form_input(array('id' => 'dpassword', 'password' => 'dpassword')); ?></td>
-                </tr>
-                <tr>
-                    <th><?php echo form_label('Password Confirm:'); ?> <?php echo form_error('dpasswordC'); ?></th>
-                    <td><?php echo form_input(array('id' => 'dpasswordC', 'passwordC' => 'dpasswordC')); ?></td>
-                </tr>
-                <tr>
-                    <th><?php echo form_label('Country:'); ?> <?php echo form_error('dcountry'); ?></th>
-                    <td><?php echo form_input(array('id' => 'dcountry', 'country' => 'dcountry')); ?></td>
-                </tr>
-                <tr>
-                    <th><?php echo form_label('City:'); ?> <?php echo form_error('dcity'); ?></th>
-                    <td><?php echo form_input(array('id' => 'dcity', 'city' => 'dcity')); ?></td>
-                </tr>
-                <tr>
-                    <th><?php echo form_label('Adress:'); ?> <?php echo form_error('dadress'); ?></th>
-                    <td><?php echo form_input(array('id' => 'dadress', 'adress' => 'dadress')); ?></td>
-                </tr>
-                <tr>
-                    <th><?php echo form_label('Tel:'); ?> <?php echo form_error('dtel'); ?></th>
-                    <td><?php echo form_input(array('id' => 'dtel', 'tel' => 'dtel')); ?></td>
-                </tr>
-                <tr>
-                    <th><?php echo form_label('Date of birth:'); ?> <?php echo form_error('ddate'); ?></th>
-                    <td><?php echo form_input(array('id' => 'ddate', 'date' => 'ddate')); ?></td>
-                </tr>
-                <tr>
-                    <td><?php echo form_submit(array('id' => 'submit', 'value' => 'Submit')); ?></td>
-                </tr>
+                <th><?php echo form_label('Name:'); ?> <?php echo form_error('dname'); ?></th>
+                <td><input type="text" name="dname" id="dname" value="<?php echo set_value('dname'); ?>" size="50" /></td>
+            </tr>
+            <tr>
+                <th><?php echo form_label('Last Name:'); ?> <?php echo form_error('dlastname'); ?></th>
+                <td><input type="text" name="dlastname" id="dlastname" value="<?php echo set_value('dlastname'); ?>" size="50" /></td>
+            </tr>
+            <tr>
+                <th><?php echo form_label('Email:'); ?> <?php echo form_error('demail'); ?></th>
+                <td><input type="text" name="demail" id="demail" value="<?php echo set_value('demail'); ?>" size="50" /></td>
+            </tr>
+            <tr>
+                <th><?php echo form_label('Password:'); ?> <?php echo form_error('dpassword'); ?></th>
+                <td><input type="text" name="dpassword" id="dpassword" value="<?php echo set_value('dpassword'); ?>" size="50" /></td>
+            </tr>
+            <tr>
+                <th><?php echo form_label('Password Confirm:'); ?> <?php echo form_error('dpasswordC'); ?></th>
+                <td><input type="text" name="dpasswordC" id="dpasswordC" value="<?php echo set_value('dpasswordC'); ?>" size="50" /></td>
+            </tr>
+            <tr>
+                <th><?php echo form_label('Country:'); ?> <?php echo form_error('dcountry'); ?></th>
+                <td><input type="text" name="dcountry" id="dcountry" value="<?php echo set_value('dcountry'); ?>" size="50" /></td>
+            </tr>
+            <tr>
+                <th><?php echo form_label('City:'); ?> <?php echo form_error('dcity'); ?></th>
+                <td><input type="text" name="dcity" id="dcity" value="<?php echo set_value('dcity'); ?>" size="50" /></td>
+            </tr>
+            <tr>
+                <th><?php echo form_label('Adress:'); ?> <?php echo form_error('dadress'); ?></th>
+                <td><input type="text" name="dadress" id="dadress" value="<?php echo set_value('dadress'); ?>" size="50" /></td>
+            </tr>
+            <tr>
+                <th><?php echo form_label('Tel:'); ?> <?php echo form_error('dtel'); ?></th>
+                <td><input type="text" name="dtel" id="dtel" value="<?php echo set_value('dtel'); ?>" size="50" /></td>
+            </tr>
+            <tr>
+                <th><?php echo form_label('Date of birth:'); ?> <?php echo form_error('ddate'); ?></th>
+                <td><input type="text" name="ddate" id="ddate" value="<?php echo set_value('ddate'); ?>" size="50" /></td>
+            </tr>
+            <tr>
+                <td><?php echo form_submit(array('id' => 'submit', 'value' => 'Submit')); ?></td>
+            </tr>
 
-                <tr>
-                    <td colspan="2" style="text-align: center;"><a href="<?php echo base_url("Category/login"); ?>">Log In</a></td>
-                </tr>
+            <tr>
+                <td colspan="2" style="text-align: center;"><a href="<?php echo base_url("Category/login"); ?>">Log In</a></td>
+            </tr>
             <?php echo form_close(); ?>
         </table>
         <?php if (isset($message)) { ?>
-       <h3 style="color:green;">Data inserted successfully</h3>
+            <h3 style="color:green;">Data inserted successfully</h3>
         <?php } ?>
     </div>
 </div>
