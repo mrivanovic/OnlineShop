d8f8c1cf5b03dd0154452d217fa19178ec7bc42f
mrivanovic <37665599+mrivanovic@users.noreply.github.com>
2018-05-29 01:51:25 +0200

    text

--- application/views/buyerFavorite.php
+++ application/views/buyerFavorite.php
@@ -1,6 +1,4 @@
-<?php $this->load->view( "sabloni/buyer_menu"); 
-
-        ?>
+<?php $this->load->view( "sabloni/buyer_menu"); ?>
 
 <?php foreach ($favorite as $element):?>
     <?php echo $element->name ?><br>
