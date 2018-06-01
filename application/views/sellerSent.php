328d4c265635fa4a9b35c686e758e3aff80cf9ee
bugarinj <39054149+bugarinj@users.noreply.github.com>
2018-06-01 13:49:25 +0200

    sent

--- application/views/sellerSent.php
+++ application/views/sellerSent.php
@@ -1,13 +1,21 @@
-<?php $this->load->view( "sabloni/profile_menu"); ?>
-    <div class="sellSent">
-        <div class="left">
-            <a href="<?php echo base_url('Account/sellerInbox');?>">Inbox</a>
-        </div>
-        <div class="right">
-            <a href="<?php echo base_url("Account/sellerSent"); ?>">Sent</a>
-        </div>
+<?php $this->load->view("sabloni/profile_menu"); ?>
+<div class="sellSent">
+    <div class="left">
+        <a href="<?php echo base_url('Account/sellerInbox'); ?>">Inbox</a>
     </div>
-    <div class="messageContent">
+    <div class="right">
+        <a href="<?php echo base_url("Account/sellerSent"); ?>">Sent</a>
+    </div>
+</div>
+<div class="messageContent">
+    <div class="sellerSentMsg">
+
+        <th><b>From:</b></th>
+        sender mail<hr><br/><br/>
+        Text<br/></br>
+        <p class="time">Time</p></br><br/>
+        <input type="button" class="openMessage" value="OPEN">
 
     </div>
+</div>
 </div>
